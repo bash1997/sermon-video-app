9c4f100755675c5a59e1a2d9e799a55eef52a918
BSH <bash_1997@outlook.com>
2022-06-08 13:00:50 -0400

    init project files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
 
 function App() {
   return (
